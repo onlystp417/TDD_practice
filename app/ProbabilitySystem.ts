841da115e85261cfb2ede40a3e468eddb60e9efa
Jin <onlystp417@gmail.com>
2026-03-18 17:52:57 +0800

    Test: Extract isRow1Hit into Reels class

--- app/ProbabilitySystem.ts
+++ app/ProbabilitySystem.ts
@@ -1,6 +1,6 @@
 export class ProbabilitySystem {
   // 讓第一條預設就連線
-  reels : Array<Array<String>> = [
+  oldReels : Array<Array<String>> = [
     ['A', 'Q', 'K'],
     ['A', 'Q', 'K'],
     ['A', 'Q', 'K'],
@@ -8,15 +8,29 @@ export class ProbabilitySystem {
     ['A', '10', 'J'],
   ]
   
+  reels: Reels = new Reels(this.oldReels)
+
   spin(betLine: string): number {
+    return this.reels.isRow1Hit() && betLine === 'L1'
+      ? 20
+      : 0
+  }
+}
+
+class Reels {
+  private reels: Array<Array<String>>
+
+  constructor(reels: Array<Array<String>>) {
+    this.reels = reels
+  }
+
+  public isRow1Hit(): boolean {
     // 判斷第一條是否連線（不轉，預設本來就連線）
     const firstLineSet = new Set<String>()
     for (let i = 0; i < this.reels.length; i++) {
       const reel = this.reels[i]
       firstLineSet.add(reel[0])
     }
-    return firstLineSet.size === 1 && betLine === 'L1'
-      ? 20
-      : 0
+    return firstLineSet.size === 1
   }
 }
