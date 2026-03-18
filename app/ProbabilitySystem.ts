489221950d4d2b491fdbc71c7ebdc7ee056b050c
Jin <onlystp417@gmail.com>
2026-03-18 17:45:28 +0800

    Test: Add fake reels

--- app/ProbabilitySystem.ts
+++ app/ProbabilitySystem.ts
@@ -1,6 +1,21 @@
 export class ProbabilitySystem {
+  // 讓第一條預設就連線
+  reels : Array<Array<String>> = [
+    ['A', 'Q', 'K'],
+    ['A', 'Q', 'K'],
+    ['A', 'Q', 'K'],
+    ['A', 'Q', 'K'],
+    ['A', '10', 'J'],
+  ]
+  
   spin(betLine: string): number {
-    return betLine === 'L1'
+    // 判斷第一條是否連線（不轉，預設本來就連線）
+    const firstLineSet = new Set<String>()
+    for (let i = 0; i < this.reels.length; i++) {
+      const reel = this.reels[i]
+      firstLineSet.add(reel[0])
+    }
+    return firstLineSet.size === 1 && betLine === 'L1'
       ? 20
       : 0
   }
