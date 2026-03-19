40b8ca7f35df3d1f4046f408eb95dd612ccbf76d
Jin <onlystp417@gmail.com>
2026-03-19 10:31:52 +0800

    Test: Lose case together

--- test/ProbabillitySystem.test.ts
+++ test/ProbabillitySystem.test.ts
@@ -15,39 +15,6 @@ describe('probability system', () => {
 		expect(sut.spin('L1')).toBe(20)
 	})
 
-	test('Row1 hit, bet L2 -> 0', () => {
-		const sut = ProbabilitySystem.create(Reels.create([
-			['A', 'Q', 'K'],
-			['A', 'Q', 'K'],
-			['A', 'Q', 'K'],
-			['A', 'Q', 'K'],
-			['A', '10', 'J'],
-		]))
-		expect(sut.spin('L2')).toBe(0)
-	})
-
-	test('Row1 hit, bet L3 -> 0', () => {
-		const sut = ProbabilitySystem.create(Reels.create([
-			['A', 'Q', 'K'],
-			['A', 'Q', 'K'],
-			['A', 'Q', 'K'],
-			['A', 'Q', 'K'],
-			['A', '10', 'J'],
-		]))
-		expect(sut.spin('L3')).toBe(0)
-	})
-
-	test('Row2 hit, bet L1 -> 0', () => {
-		const sut = ProbabilitySystem.create(Reels.create([
-			['A', 'Q', 'K'],
-			['A', 'Q', 'K'],
-			['A', 'Q', 'K'],
-			['A', 'Q', 'K'],
-			['10', 'Q', 'J'],
-		]))
-		expect(sut.spin('L1')).toBe(0)
-	})
-
 	test('Row2 hit, bet L2 -> 20', () => {
 		const sut = ProbabilitySystem.create(Reels.create([
 			['A', 'Q', 'K'],
@@ -57,39 +24,6 @@ describe('probability system', () => {
 			['10', 'Q', 'J'],
 		]))
 		expect(sut.spin('L2')).toBe(20)
-	})
-
-		test('Row2 hit, bet L3 -> 0', () => {
-		const sut = ProbabilitySystem.create(Reels.create([
-			['A', 'Q', 'K'],
-			['A', 'Q', 'K'],
-			['A', 'Q', 'K'],
-			['A', 'Q', 'K'],
-			['10', 'Q', 'J'],
-		]))
-		expect(sut.spin('L3')).toBe(0)
-	})
-
-		test('Row3 hit, bet L1 -> 0', () => {
-		const sut = ProbabilitySystem.create(Reels.create([
-			['A', 'Q', 'K'],
-			['A', 'Q', 'K'],
-			['A', 'Q', 'K'],
-			['A', 'Q', 'K'],
-			['10', 'J', 'K'],
-		]))
-		expect(sut.spin('L1')).toBe(0)
-	})
-
-	test('Row3 hit, bet L2 -> 0', () => {
-		const sut = ProbabilitySystem.create(Reels.create([
-			['A', 'Q', 'K'],
-			['A', 'Q', 'K'],
-			['A', 'Q', 'K'],
-			['A', 'Q', 'K'],
-			['10', 'J', 'K'],
-		]))
-		expect(sut.spin('L2')).toBe(0)
 	})
 
 	test('Row3 hit, bet L3 -> 20', () => {
@@ -102,4 +36,25 @@ describe('probability system', () => {
 		]))
 		expect(sut.spin('L3')).toBe(20)
 	})
+
+	// 把輸的測項統一寫在一起
+	test('Bet line is not match hit row -> 0', () => {
+		// arrange
+		const row1Hit = [
+			['A', 'Q', 'K'],
+			['A', 'Q', 'K'],
+			['A', 'Q', 'K'],
+			['A', 'Q', 'K'],
+			['A', '10', 'J'],
+		]
+		const betLineIsNotLine1 = 'L2'
+		const sut = ProbabilitySystem.create(Reels.create(row1Hit))
+
+		// action, basicly 1 line
+		const result = sut.spin(betLineIsNotLine1)
+
+		// assertion
+		expect(result).toBe(0)
+	})
 })
+
