9030436f45c9267dc117a3b69b8fd1b606010291
Jin <onlystp417@gmail.com>
2026-03-19 08:12:08 +0800

    Test: Add L3 test case

--- test/ProbabillitySystem.test.ts
+++ test/ProbabillitySystem.test.ts
@@ -26,6 +26,17 @@ describe('probability system', () => {
 		expect(sut.spin('L2')).toBe(0)
 	})
 
+	test('Row1 hit, bet L3 -> 0', () => {
+		const sut = ProbabilitySystem.create(Reels.create([
+			['A', 'Q', 'K'],
+			['A', 'Q', 'K'],
+			['A', 'Q', 'K'],
+			['A', 'Q', 'K'],
+			['A', '10', 'J'],
+		]))
+		expect(sut.spin('L3')).toBe(0)
+	})
+
 	test('Row2 hit, bet L1 -> 0', () => {
 		const sut = ProbabilitySystem.create(Reels.create([
 			['A', 'Q', 'K'],
@@ -46,5 +57,49 @@ describe('probability system', () => {
 			['10', 'Q', 'J'],
 		]))
 		expect(sut.spin('L2')).toBe(20)
+	})
+
+		test('Row2 hit, bet L3 -> 0', () => {
+		const sut = ProbabilitySystem.create(Reels.create([
+			['A', 'Q', 'K'],
+			['A', 'Q', 'K'],
+			['A', 'Q', 'K'],
+			['A', 'Q', 'K'],
+			['10', 'Q', 'J'],
+		]))
+		expect(sut.spin('L3')).toBe(0)
+	})
+
+		test('Row3 hit, bet L1 -> 0', () => {
+		const sut = ProbabilitySystem.create(Reels.create([
+			['A', 'Q', 'K'],
+			['A', 'Q', 'K'],
+			['A', 'Q', 'K'],
+			['A', 'Q', 'K'],
+			['10', 'J', 'K'],
+		]))
+		expect(sut.spin('L1')).toBe(0)
+	})
+
+	test('Row3 hit, bet L2 -> 0', () => {
+		const sut = ProbabilitySystem.create(Reels.create([
+			['A', 'Q', 'K'],
+			['A', 'Q', 'K'],
+			['A', 'Q', 'K'],
+			['A', 'Q', 'K'],
+			['10', 'J', 'K'],
+		]))
+		expect(sut.spin('L2')).toBe(0)
+	})
+
+	test('Row3 hit, bet L3 -> 20', () => {
+		const sut = ProbabilitySystem.create(Reels.create([
+			['A', 'Q', 'K'],
+			['A', 'Q', 'K'],
+			['A', 'Q', 'K'],
+			['A', 'Q', 'K'],
+			['10', 'J', 'K'],
+		]))
+		expect(sut.spin('L3')).toBe(20)
 	})
 })
